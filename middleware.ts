a46c7bc725e305470b896807f9df1c1b5583b595
Razan Doughman <rhd18@mail.aub.edu>
2025-10-26 18:26:15 +0200

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -3,3 +3,8 @@ export { default } from "next-auth/middleware"
 export const config = {
   matcher: ["/dashboard/:path*", "/workouts/:path*", "/progress/:path*"],
 }
+
+
+export function middleware() {
+  return;
+}
